26f0f370da5f0a4c51b1a0ebebbe6e1b075ce93a
manish-dalal <manish.dalal@daffodilsw.com>
2020-02-28 01:24:27 +0530

    Updates

---
+++ precache-manifest.f0928eed5cd5724992224a54dfbd51ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51b3cebc3c35c2ec99c133b4c9dbb605",
+    "url": "/video-react-example/index.html"
+  },
+  {
+    "revision": "ba0e61e4fa221336ec92",
+    "url": "/video-react-example/static/css/2.e5f752a8.chunk.css"
+  },
+  {
+    "revision": "352a203b3cf4cd22db77",
+    "url": "/video-react-example/static/css/main.1894eeb4.chunk.css"
+  },
+  {
+    "revision": "ba0e61e4fa221336ec92",
+    "url": "/video-react-example/static/js/2.162e91ad.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/video-react-example/static/js/2.162e91ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "352a203b3cf4cd22db77",
+    "url": "/video-react-example/static/js/main.aa2c24c0.chunk.js"
+  },
+  {
+    "revision": "dd0cd20faf793b0b6276",
+    "url": "/video-react-example/static/js/runtime-main.e90631f8.js"
+  }
+]);
